efc65639251f4d3b60411f516e4655b7ee313af1
dana <danah.albahusain@gmail.com>
2020-08-14 18:52:42 +0300

    More changes to the look of the website + attempting to add images.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views
 
 urlpatterns = [
@@ -8,4 +9,4 @@ urlpatterns = [
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
