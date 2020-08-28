abd410f7e9e7c27fade2a37958615b12bda28038
dana <danah.albahusain@gmail.com>
2020-08-28 13:05:16 +0300

    Changed logo, colors, and homepage.

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,9 @@ from django.core.files.storage import FileSystemStorage
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/home.html', {'posts': posts})
+def blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/blog.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
